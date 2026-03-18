36115138b84e8342463366505cd2e36e71e8d56c
S <2479645@cognizant.com>
2026-03-18 11:57:36 +0530

    added config file

---
+++ src/main/java/com/example/demo/Configuration/SecurityConfig.java
@@ -0,0 +1,20 @@
+package com.example.demo.Configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public UserDetailsService userDetailsService(){
+        UserDetails user= User.builder()
+                .username("user")
+                .password("{noop}jesse")
+                .build();
+        return new InMemoryUserDetailsManager(user);
+    }
+}
